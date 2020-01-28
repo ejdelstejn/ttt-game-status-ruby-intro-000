07f13a21c9fb42f3a223ad1f661268a65ab634b3
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-01-28 03:19:46 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,4 +1,3 @@
-# Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
@@ -16,26 +15,54 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
 
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-
-    position_1 = board[win_index_1] # load the value of the board at win_index_1
-    position_2 = board[win_index_2] # load the value of the board at win_index_2
-    position_3 = board[win_index_3] # load the value of the board at win_index_3
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination
+      return win_combo
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combination
+      return win_combo
     end
+  }
+  return false
+end
+
+def full?(board)
+  board.all? {|index| index == "X" || index == "O"}
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
   end
-  board.all? do |x|
-    x.nil? || x == " "
+end
+
+def over?(board)
+  if won?(board) || draw?(board)
+    return true
+  else
+    return false
   end
-  board.all? do |x|
-    !(x.nil? || x == " ")
+end
+
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
   end
 end
